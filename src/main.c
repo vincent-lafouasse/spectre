e0beaa5ba2db1097115be123fc30754c1f8b028f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-31 13:32:17 +0100

    play wav

--- src/main.c
+++ src/main.c
@@ -1,23 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #include <complex.h>
-#include <math.h>
-#include <stdio.h>
 
-#include <fftw3.h>
 #include <raylib.h>
 
 #define N 64
 
 int main(void)
 {
-    // better than straight malloc as it aligns for potential SIMD
-    // https://www.fftw.org/fftw3_doc/SIMD-alignment-and-fftw_005fmalloc.html
-    float* in = fftwf_alloc_real(N);
-
     InitWindow(100, 100, "title");
+    InitAudioDevice();
 
+    Sound sound = LoadSound("audio/Bbmaj9.wav");
+    PlaySound(sound);
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+    UnloadSound(sound);
+    CloseAudioDevice();
     CloseWindow();
-    fftwf_free(in);
-
-    return 0;
 }
