f341aa38138c1b0d6c5d47d27763d6e7e99224ab
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-10 00:14:27 +0100

    implement `pass_sample_tx()`

--- src/audio_callback.c
+++ src/audio_callback.c
@@ -2,6 +2,11 @@
 
 static LockFreeQueueProducer* sample_tx = NULL;
 
+void pass_sample_tx(LockFreeQueueProducer* sample_tx_passed)
+{
+    sample_tx = sample_tx_passed;
+}
+
 void pull_samples_from_audio_thread(void* buffer, unsigned int frames)
 {
     if (sample_tx == NULL) {
