9f19b96551045dcec9b58fce56677b5fc5fe4b88
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-10 00:52:48 +0100

    refactor

--- src/audio_callback.c
+++ src/audio_callback.c
@@ -1,20 +1,41 @@
 #include "audio_callback.h"
 
 #include <assert.h>
+#include <stdatomic.h>
+#include <stdlib.h>
+
 #include "LockFreeQueue.h"
 
-static LockFreeQueueProducer* sample_tx = NULL;
+#define MONO_BUFFER_SIZE 1024
 
-void pass_sample_tx(LockFreeQueueProducer* sample_tx_passed)
+float mono_buffer[MONO_BUFFER_SIZE];
+
+static _Atomic(LockFreeQueueProducer*) s_sample_tx = NULL;
+
+void init_audio_processor(LockFreeQueueProducer* sample_tx_passed)
 {
-    sample_tx = sample_tx_passed;
+    atomic_store_explicit(&s_sample_tx, sample_tx_passed, memory_order_release);
 }
+
+void deinit_audio_processor(void) {}
 
 // always interleaved stereo
 void pull_samples_from_audio_thread(void* buffer, unsigned int frames)
 {
+    LockFreeQueueProducer* sample_tx =
+        atomic_load_explicit(&s_sample_tx, memory_order_acquire);
     assert(sample_tx != NULL);
 
     const float* samples = (const float*)buffer;
-    (void)clfq_push_partial(sample_tx, samples, 2 * frames, 2);
+
+    while (frames != 0) {
+        const SizeType to_pull =
+            frames <= MONO_BUFFER_SIZE ? frames : MONO_BUFFER_SIZE;
+
+        for (SizeType i = 0; i < to_pull; i++) {
+            mono_buffer[i] = 0.5 * (samples[2 * i] + samples[2 * i + 1]);
+        }
+
+        (void)clfq_push_partial(sample_tx, mono_buffer, to_pull, 1);
+    }
 }
