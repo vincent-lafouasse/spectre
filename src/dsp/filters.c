c90bcc7af865b23f3a7d7c80bb011c7ec0d6e0d9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-12 00:40:27 +0100

    refactor filter implementation

--- src/dsp/filters.c
+++ src/dsp/filters.c
@@ -20,13 +20,20 @@ void filter_lpf_process(OnePoleFilter* restrict f,
                         SizeType size)
 {
     const float a = f->alpha;
+    const float b = 1.0f - a;
+
+    float x_prev = f->x_prev;
+    float y_prev = f->y_prev;
 
     for (SizeType i = 0; i < size; ++i) {
         const float x_n_cache = data[i];
-        data[i] = (1.0f - a) * data[i] + a * f->y_prev;
-        f->x_prev = x_n_cache;
-        f->y_prev = data[i];
+        data[i] = b * data[i] + a * y_prev;
+        x_prev = x_n_cache;
+        y_prev = data[i];
     }
+
+    f->x_prev = x_prev;
+    f->y_prev = y_prev;
 }
 
 // y[n] = a * y[n-1] + a * (x[n] - x[n-1])
@@ -36,10 +43,16 @@ void filter_hpf_process(OnePoleFilter* restrict f,
 {
     const float a = f->alpha;
 
+    float x_prev = f->x_prev;
+    float y_prev = f->y_prev;
+
     for (SizeType i = 0; i < size; ++i) {
         const float x_n_cache = data[i];
-        data[i] = a * f->y_prev + a * (data[i] - f->x_prev);
-        f->x_prev = x_n_cache;
-        f->y_prev = data[i];
+        data[i] = a * (y_prev + data[i] - x_prev);
+        x_prev = x_n_cache;
+        y_prev = data[i];
     }
+
+    f->x_prev = x_prev;
+    f->y_prev = y_prev;
 }
