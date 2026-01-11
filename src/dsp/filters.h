c75f693923cea2f2f699054e42061ea8da9b1a7d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-12 00:25:26 +0100

    make pointers restrict to allow optimizations

--- src/dsp/filters.h
+++ src/dsp/filters.h
@@ -12,7 +12,11 @@ typedef struct {
 OnePoleFilter filter_init(float cutoff_frequency, float sample_rate);
 
 // y[n] = (1 - a) * x[n] + a * y[n-1]
-void filter_lpf_process(OnePoleFilter* f, float* data, SizeType size);
+void filter_lpf_process(OnePoleFilter* restrict f,
+                        float* restrict data,
+                        SizeType size);
 
 // y[n] = a * y[n-1] + a * (x[n] - x[n-1])
-void filter_hpf_process(OnePoleFilter* f, float* data, SizeType size);
+void filter_hpf_process(OnePoleFilter* restrict f,
+                        float* restrict data,
+                        SizeType size);
