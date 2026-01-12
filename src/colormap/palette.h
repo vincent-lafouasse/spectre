8ee96f87cf4275ca40ae3b2dd20b38919511dbe3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-12 06:11:32 +0100

    format

--- src/colormap/palette.h
+++ src/colormap/palette.h
@@ -2,6 +2,18 @@
 
 #include <raylib.h>
 
-#define BACKGROUND_COLOR CLITERAL(Color){10, 10, 10, 255}
-#define GRID_COLOR CLITERAL(Color){42, 42, 42, 255}
-#define TEXT_COLOR CLITERAL(Color){176, 176, 176, 255}
+#define BACKGROUND_COLOR \
+    CLITERAL(Color)      \
+    {                    \
+        10, 10, 10, 255  \
+    }
+#define GRID_COLOR      \
+    CLITERAL(Color)     \
+    {                   \
+        42, 42, 42, 255 \
+    }
+#define TEXT_COLOR         \
+    CLITERAL(Color)        \
+    {                      \
+        176, 176, 176, 255 \
+    }
