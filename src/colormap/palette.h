6c440ad2dcf987d1e4000658c8c9ae785f77e064
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-13 01:34:34 +0100

    fuck sake, clang-format

--- src/colormap/palette.h
+++ src/colormap/palette.h
@@ -2,6 +2,8 @@
 
 #include <raylib.h>
 
+// clang-format off
 #define BACKGROUND_COLOR CLITERAL(Color){10, 10, 10, 255}
 #define GRID_COLOR CLITERAL(Color){42, 42, 42, 255}
 #define TEXT_COLOR CLITERAL(Color){176, 176, 176, 255}
+// clang-format on
