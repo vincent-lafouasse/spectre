ca5ab7c70a844c2768491c4d6321926f2002c359
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-19 00:19:47 +0200

    manually cast

--- src/dsp/window.c
+++ src/dsp/window.c
@@ -8,7 +8,8 @@ void make_hann_window(float* window, SizeType N)
     assert(N > 0);
 
     for (SizeType i = 0; i < N; i++) {
-        window[i] = 0.5f * (1.0f - cosf(2.0f * PI * i / (N - 1)));
+        const float fraction = (float)i / (float)(N - 1);
+        window[i] = 0.5f * (1.0f - cosf(2.0f * PI * fraction));
     }
 }
 
