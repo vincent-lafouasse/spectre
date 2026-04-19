3d1fbb69a34d94a240f2812f30152e67ee6cb6ca
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-19 18:11:11 +0200

    clarify in comments the reasoning for u32 being a suitable size type

--- src/core/platform_check.h
+++ src/core/platform_check.h
@@ -5,16 +5,23 @@
 
 // checks for portability
 
-// i will always use u32 for size_t
-// the pointer check is only for atomic pointers
+// (int is i32) && (atomic<int> is always lock-free)
+//   => atomic<i32> is always lock-free
+//   => atomic<u32> is always lock-free
+//   => u32 is a suitable size type
 
 _Static_assert(sizeof(int) == 4,
                "This codebase strictly requires 4-byte integers.");
 _Static_assert(
     ATOMIC_INT_LOCK_FREE == 2,
     "32-bit operations are not natively lock-free on this platform.");
-// now that we are assured that u32 are always lock-free
+
+// if the above assertions passed, then u32 is a suitable size type
 typedef uint32_t SizeType;
 
+// atomic pointers being lock-free is nice for CAS operations but i don't
+// actually need that so i won't run this assertion
+/*
 _Static_assert(ATOMIC_POINTER_LOCK_FREE == 2,
                "Pointer-sized operations are not natively lock-free.");
+*/
