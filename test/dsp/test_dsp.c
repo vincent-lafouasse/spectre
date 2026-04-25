70a2379713d6a45db0f2f492381a0a7ff45baaf9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-26 00:21:32 +0200

    remove dummy test

--- test/dsp/test_dsp.c
+++ test/dsp/test_dsp.c
@@ -8,15 +8,9 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-void test_dummy(void)
-{
-    float window[5];
-    window_make_hann(window, 5);
-}
 
 int main(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_dummy);
     return UNITY_END();
 }
