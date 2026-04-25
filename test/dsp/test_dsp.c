233814d64389077cf285c52e0b01a7ae7ae94fb4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-26 00:26:51 +0200

    test that Hann endpoints are strictly 0

--- test/dsp/test_dsp.c
+++ test/dsp/test_dsp.c
@@ -8,9 +8,21 @@
 void setUp(void) {}
 void tearDown(void) {}
 
+void test_hann_endpoints_are_zero(void)
+{
+    enum { N = 2048 };
+    float window[N];
+    window_make_hann(window, N);
+
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, window[0]);
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, window[N - 1]);
+}
 
 int main(void)
 {
     UNITY_BEGIN();
+
+    RUN_TEST(test_hann_endpoints_are_zero);
+
     return UNITY_END();
 }
