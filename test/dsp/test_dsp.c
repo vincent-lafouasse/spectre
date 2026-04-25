7faf2a0243ffba64f3178c92d4da0c383cb5fdf6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-26 00:49:39 +0200

    check that the Hann window is strictly symmetric

--- test/dsp/test_dsp.c
+++ test/dsp/test_dsp.c
@@ -18,11 +18,23 @@ void test_hann_endpoints_are_zero(void)
     TEST_ASSERT_EQUAL_FLOAT(0.0f, window[N - 1]);
 }
 
+void test_hann_is_symmetric(void)
+{
+    enum { N = 2048 };
+    float window[N];
+    window_make_hann(window, N);
+
+    for (SizeType i = 0; i < N / 2; ++i) {
+        TEST_ASSERT_EQUAL_FLOAT(window[i], window[N - 1 - i]);
+    }
+}
+
 int main(void)
 {
     UNITY_BEGIN();
 
     RUN_TEST(test_hann_endpoints_are_zero);
+    RUN_TEST(test_hann_is_symmetric);
 
     return UNITY_END();
 }
