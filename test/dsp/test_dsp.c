dbe1fb15b1025c3ae2f832b02e881e9a7e5f4ba1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-26 02:19:38 +0200

    remove pointless Hann value tests

--- test/dsp/test_dsp.c
+++ test/dsp/test_dsp.c
@@ -8,33 +8,9 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-void test_hann_endpoints_are_zero(void)
-{
-    enum { N = 2048 };
-    float window[N];
-    window_make_hann(window, N);
-
-    TEST_ASSERT_EQUAL_FLOAT(0.0f, window[0]);
-    TEST_ASSERT_EQUAL_FLOAT(0.0f, window[N - 1]);
-}
-
-void test_hann_is_symmetric(void)
-{
-    enum { N = 2048 };
-    float window[N];
-    window_make_hann(window, N);
-
-    for (SizeType i = 0; i < N / 2; ++i) {
-        TEST_ASSERT_EQUAL_FLOAT(window[i], window[N - 1 - i]);
-    }
-}
-
 int main(void)
 {
     UNITY_BEGIN();
-
-    RUN_TEST(test_hann_endpoints_are_zero);
-    RUN_TEST(test_hann_is_symmetric);
 
     return UNITY_END();
 }
