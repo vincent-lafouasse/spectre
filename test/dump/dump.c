28f85efa218faf5842e89a6706b845d64369fffd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-27 22:32:34 +0200

    check that wav path ends with .wav, otherwise reject (for now)

--- test/dump/dump.c
+++ test/dump/dump.c
@@ -1,8 +1,20 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define DR_WAV_IMPLEMENTATION
 #include "dr_wav.h"
+
+static bool str_ends_with(const char* s, const char* suffix)
+{
+    if (strlen(s) > strlen(suffix)) {
+        return false;
+    }
+
+    const char* s_suffix = s + strlen(s) - strlen(suffix);
+
+    return (strcmp(s_suffix, suffix) == 0);
+}
 
 int main(int ac, char* av[])
 {
@@ -14,6 +26,11 @@ int main(int ac, char* av[])
     const char* input_wav = av[1];
     const char* output_pgm = av[2];
     (void)output_pgm;
+
+    if (!str_ends_with(input_wav, ".wav")) {
+        fprintf(stderr, "Only wav files are supported for now\n");
+        return 1;
+    }
 
     drwav wav;
     if (!drwav_init_file(&wav, input_wav, NULL)) {
